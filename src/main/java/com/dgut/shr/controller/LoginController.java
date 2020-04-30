bc86d1c6d7afd6940ce8dff1537fee78fe09c9cc
a991817 <981502062@qq.com>
2020-04-30 14:07:16 +0800

    增加用户身份

--- src/main/java/com/dgut/shr/controller/LoginController.java
+++ src/main/java/com/dgut/shr/controller/LoginController.java
@@ -1,6 +1,9 @@
 package com.dgut.shr.controller;
 
 import com.dgut.shr.dto.EmployeeDto;
+import com.dgut.shr.javaBean.Apply;
+import com.dgut.shr.service.ApplyService;
+import com.dgut.shr.service.DepartmentService;
 import com.dgut.shr.service.EmployeeService;
 import com.dgut.shr.service.sys.LoginService;
 import com.dgut.shr.service.sys.RedisService;
@@ -24,6 +27,10 @@ public class LoginController {
     LoginService loginService;                    
     @Autowired
     RedisService redisService;
+    @Autowired
+    DepartmentService departmentService;
+    @Autowired
+    ApplyService applyService;
     public static final String TOKEN = "shr_token";
 
     private static final int COOKIE_MAX_AGE =  60*5     ;
@@ -46,15 +53,13 @@ public class LoginController {
      * @return
      */
     @RequestMapping("/doLogin")
-    public String login(String username , String password, Model model, HttpSession httpSession, HttpServletResponse response){
+    public String doLogin(String username , String password,String role, Model model, HttpSession httpSession, HttpServletResponse response){
         //其实username是邮箱
-        String adminUserName = loginService.isAdmin(username, password);
+        String adminUserName = loginService.isAdmin(username, password,role);
         if (!StringUtils.isEmpty(adminUserName)) {
             //登陆成功
-            model.addAttribute("msg", "success");
-            model.addAttribute("username", "管理员");
+            model.addAttribute("username", adminUserName);
             httpSession.setAttribute("username","管理员");
-
             String token = createToken();
             Cookie cookie = new Cookie(TOKEN,token);
             cookie.setPath("/");
@@ -91,7 +96,21 @@ public class LoginController {
      * @return
      */
     @RequestMapping("/toMain")
-    public String toMain(){
+    public String toMain(Model model){
+        EmployeeDto employeeDto = new EmployeeDto();
+//        在职
+        employeeDto.setWorkState(1);
+        int empNums = employeeService.countAllEmployee(employeeDto);
+        int depNums = departmentService.countDepartment();
+        int applyAll = applyService.countApply(new Apply());
+        Apply queryApply = new Apply();
+//        未审批
+        queryApply.setState(0);
+        int applyNotApproved = applyService.countApply(queryApply);
+        model.addAttribute("empNums",empNums);
+        model.addAttribute("depNums",depNums);
+        model.addAttribute("applyAll",applyAll);
+        model.addAttribute("applyNotApproved",applyNotApproved);
         return "page/main";
     }
 
